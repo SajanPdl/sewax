19be3da01cc120df231dff652bd0ae6ac6f76659
Sajan Poudel <108209102+SajanPdl@users.noreply.github.com>
2025-12-05 16:35:04 +0545

    feat(agency-theme): Improve layout and dependencies
    Update package dependencies to the latest versions and refactor CSS for better organization and maintainability in the agency theme. This includes adjustments for RTL support and general styling improvements.

--- themes/agency/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-
-
-$(document).ready(function (){
-
-    $(document).on('click', '.click-nav-right-icon', function() {
-        $(".show-nav-content, .nav-right-content").toggleClass("show");
-    });
-
-});
